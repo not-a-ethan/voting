58e9a0df9b5cb15990c2415703ba9d2339d66f8f
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-01-30 21:52:38 -0500

    Worked on adding a chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import Chart from 'chart.js/auto';
+
 import { headers } from 'next/headers';
+import Script from 'next/script'
 
 export default async function Page() {
     const headerList = headers();
@@ -18,6 +21,37 @@ export default async function Page() {
                 <p>Hambuger votes: {await body["hamburger"]} ({await body["hamburger percent"]}%)</p>
                 <p>Hotdog votes: {await body["hotdog"]} ({await body["hotdog percent"]}%)</p>
             </div>
+
+            <canvas id="myChart"></canvas> 
+
+            <script>
+                const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+                const yValues = [55, 49, 44, 24, 15];
+                const barColors = [
+                    "#b91d47",
+                    "#00aba9",
+                    "#2b5797",
+                    "#e8c3b9",
+                    "#1e7145"
+                ];
+
+                new Chart("myChart", {
+                    type: "pie",
+                    data: {
+                        labels: xValues,
+                        datasets: [{
+                        backgroundColor: barColors,
+                        data: yValues
+                        }]
+                    },
+                    options: {
+                        title: {
+                        display: true,
+                        text: "World Wide Wine Production"
+                        }
+                    }
+                });
+            </script>
         </div>
     )
 }
