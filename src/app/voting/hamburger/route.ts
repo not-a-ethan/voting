7877df272ef88e45f7d96ebe3b5c7fab0581996c
Ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-02-02 16:56:58 -0500

    minor changes

--- src/app/voting/hamburger/route.ts
+++ src/app/voting/hamburger/route.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from 'next/server'
 
 import sql from "../postgres";
 
-export async function GET(req: NextRequest, res: NextResponse) {
-    const hamburger = await sql`SELECT vote FROM votes WHERE vote=hamburger;`
-    const total = await sql`SELECT COUNT(*) FROM votes`
+export async function GET() {
+    const hamburger = await sql`SELECT vote FROM votes WHERE vote=hamburger;`;
+    const total = await sql`SELECT COUNT(*) FROM votes;`;
 
     return NextResponse.json(
         JSON.stringify({
