0522bca809152debf365dc72be78fb5c1c580481
Ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-03-09 21:52:41 -0400

    Made time of votes display

--- src/app/voting/hamburger/route.ts
+++ src/app/voting/hamburger/route.ts
@@ -3,13 +3,20 @@ import { NextRequest, NextResponse } from 'next/server'
 import sql from "../postgres";
 
 export async function GET() {
-    const hamburger = await sql`SELECT vote FROM votes WHERE vote=hamburger;`;
+    const hamburger = await sql`SELECT * FROM votes WHERE vote='hamburger';`;
     const total = await sql`SELECT COUNT(*) FROM votes;`;
+
+    const times = [];
+
+    for (let i = 0; i < hamburger.length; i++) {
+        times.push(hamburger[i]["id"])
+    }
 
     return NextResponse.json(
         JSON.stringify({
             "votes": hamburger.length,
-            "percent": (hamburger.length / total[0]['total']) * 100
+            "percent": (hamburger.length / total[0]['total']) * 100,
+            "times": times
         }),
         { status: 200 }
     )
--- src/app/voting/hotdog/route.ts
+++ src/app/voting/hotdog/route.ts
@@ -3,13 +3,22 @@ import { NextRequest, NextResponse } from 'next/server'
 import sql from "../postgres";
 
 export async function GET() {
-    const hotdog = await sql`SELECT vote FROM votes WHERE vote=hotdog;`
-    const total = await sql`SELECT COUNT(*) FROM votes`
+    const hotdog = await sql`SELECT * FROM votes WHERE vote='hotdog';`;
+    const total = await sql`SELECT COUNT(*) FROM votes`;
+
+    console.log(hotdog)
+
+    const times = [];
+
+    for (let i = 0; i < hotdog.length; i++) {
+        times.push(hotdog[i]["id"])
+    }
 
     return NextResponse.json(
         JSON.stringify({
             "votes": hotdog.length,
-            "percent": (hotdog.length / total[0]['total']) * 100
+            "percent": (hotdog.length / total[0]['total']) * 100,
+            "times": times
         }),
         { status: 200 }
     )
