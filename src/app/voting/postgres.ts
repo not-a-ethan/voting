6962588033eddfd2766cc79125234ca61af145f9
Ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-05-01 01:50:55 +0000

    Update postgres.ts

--- src/app/voting/postgres.ts
+++ src/app/voting/postgres.ts
@@ -1,11 +1,5 @@
 import postgres from 'postgres'
 
-const sql = postgres({
-    host                 : process.env.sqlHost,        // Postgres ip address[s] or domain name[s]
-    port                 : 5432,                       // Postgres server port[s]
-    database             : process.env.sqlName,        // Name of database to connect to
-    username             : process.env.sqlUsername,    // Username of database user
-    password             : process.env.sqlPassword,    // Password of database user
-})
+const sql = postgres(process.env.url)
 
 export default sql
